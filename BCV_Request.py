bbf7e0b5d05b5fba490c15bab2f84ca8d2c9b2cd
gonzalezmfrank <gonzalezmfrank@gmail.com>
2026-08-29 11:48:04 -0400

    Para Depurar el archivo Inicial del request a BCV

--- BCV_Request.py
+++ BCV_Request.py
@@ -7,26 +7,20 @@ from KEY import ARCHIVO2
 
 from pathlib import Path
 
-print("el API Key es :",CLAVE)
+#print("el API Key es :",CLAVE)
 ##print("el URL es :",URL)
 
 bcv = requests.get(
     URL,
     headers={'X-API-Key': CLAVE},
 )
-#print(bcv.status)
+
 data = bcv.json()
 
-##ARCHIVO = os.getcwd()+ARCHIVO
-ARCHIVO2 =str(Path.cwd())+"\\" + ARCHIVO2
-
-print("el directorio es :",ARCHIVO2)
-
-ARCHIVO = ARCHIVO2
+ARCHIVO =str(Path.cwd())+"\\" + ARCHIVO2
 
 try:
 	with open(ARCHIVO,"r+", encoding="utf-8") as f:
-     #		datos = json.load(f)
 		f.seek(0)
 		f.truncate() 
 		json.dump(data, f, indent=4, ensure_ascii=False)
